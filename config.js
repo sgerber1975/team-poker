2fdcf8807f293e40fcefe13fe41bd0df6e1a4b90
sgerber1975 <sgerber1975@users.noreply.github.com>
2026-02-27 05:50:41 +0000

    deploy: d3a6067d967a9b5d356e15e27dbfade760169198

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.FIREBASE_CONFIG = {
-  apiKey: "",
-  projectId: "",
-  databaseURL: ""
+  apiKey: "Name: FIREBASE_API_KEY → AIzaSyDtBEH5k1b-Q2E3zXTDgEC5BuqPUO1l7tA",
+  projectId: "Name: FIREBASE_PROJECT_ID → team-poker-8b2f0",
+  databaseURL: "Name: FIREBASE_DATABASE_URL → https://team-poker-8b2f0-default-rtdb.asia-southeast1.firebasedatabase.app"
 };
